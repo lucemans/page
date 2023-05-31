edc468d185ee7fb3ed43e75107c3d477d8317430
Luc <luc@lucemans.nl>
2023-05-31 04:26:25 +0000

    Interesting build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default async () => {
     // This fetch will run on the server during `next build`
-    const response = await fetch('https://miguel.build/api/location');
+    const response = await fetch(
+        'http://worldtimeapi.org/api/timezone/Europe/Amsterdam',
+        {
+            next: { revalidate: 10 },
+        }
+    );
     const data = await response.json();
 
     return <main>...{JSON.stringify(data)}</main>;
